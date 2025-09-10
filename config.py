32ee5ba68396feaa3315f68558cebeb136defd3f
dragon28rus <dragon28rus@gmail.com>
2025-09-11 00:59:10 +0900

    comit

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 from decouple import config
 
 # Токен Telegram-бота
-BOT_TOKEN = config('BOT_TOKEN', env_file='env/.env')
+BOT_TOKEN = config('BOT_TOKEN')
 
 # Настройки BGBilling API
-BGBILLING_API_URL = config('BGBILLING_API_URL', env_file='env/.env')
-BGBILLING_AUTH = (config('BGBILLING_USERNAME', env_file='env/.env'), config('BGBILLING_PASSWORD', env_file='env/.env'))
+BGBILLING_API_URL = config('BGBILLING_API_URL')
+BGBILLING_AUTH = (config('BGBILLING_USERNAME'), config('BGBILLING_PASSWORD'))
 
 # Настройки вебхука
-WEBHOOK_URL = config('WEBHOOK_URL', env_file='env/.env')
-WEBHOOK_PATH = config('WEBHOOK_PATH', env_file='env/.env')
+WEBHOOK_URL = config('WEBHOOK_URL')
+WEBHOOK_PATH = config('WEBHOOK_PATH')
 
 # Chat ID технической поддержки
-SUPPORT_CHAT_ID = config('SUPPORT_CHAT_ID', cast=int, env_file='env/.env')
+SUPPORT_CHAT_ID = config('SUPPORT_CHAT_ID', cast=int)
