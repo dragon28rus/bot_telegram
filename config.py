80bc79ab699b063b8956a8c31358fa719998c108
NETADMIN\User <dragon28rus@gmail.com>
2025-09-11 10:38:41 +0900

    comit

--- config.py
+++ config.py
@@ -1,18 +1,28 @@
-from decouple import config
+import os
+from dotenv import load_dotenv
+
+# Загружаем переменные из .env
+load_dotenv()
 
 # Токен Telegram-бота
-BOT_TOKEN = config('BOT_TOKEN')
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# SQLite Database
+DB_PATH = os.getenv("DB_PATH", "./db.sqlite3")
 
 # Настройки BGBilling API
-BGBILLING_API_URL = config('BGBILLING_API_URL')
-BGBILLING_AUTH = (config('BGBILLING_USERNAME'), config('BGBILLING_PASSWORD'))
+BGBILLING_API_URL = os.getenv('BGBILLING_API_URL')
+BGBILLING_AUTH = (os.getenv('BGBILLING_USERNAME'), os.getenv('BGBILLING_PASSWORD'))
 
 # Настройки вебхука
-WEBHOOK_URL = config('WEBHOOK_URL')
-WEBHOOK_PATH = config('WEBHOOK_PATH')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
+WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/webhook")
 
 # Chat ID технической поддержки
-SUPPORT_CHAT_ID = config('SUPPORT_CHAT_ID', cast=int)
+SUPPORT_CHAT_ID = int(os.getenv("SUPPORT_CHAT_ID", "0"))
 
 # Токен для авторизации биллинг API
-BILLING_API_TOKEN = config('BILLING_API_TOKEN')
+BILLING_API_TOKEN = os.getenv('BILLING_API_TOKEN')
+
+# Logging
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
