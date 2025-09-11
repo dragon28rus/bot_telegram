d6abbb87cf2476e7ee56460275e748e499ad14ed
NETADMIN\User <dragon28rus@gmail.com>
2025-09-11 10:51:31 +0900

    comit

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,8 @@
-from aiogram import Dispatcher
 from .auth import router as auth_router
-from .user import router as user_router
-from .support import router as support_router
 from .check import router as check_router
+from .support import router as support_router
+from .user import router as user_router
+
 from .billing import handle_billing_notification, handle_broadcast_notification
 
 def register_handlers(dp: Dispatcher):
