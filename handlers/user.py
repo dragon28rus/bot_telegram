dbaf2028f04db49ba99602679396b8a99fec13dc
NETADMIN\User <dragon28rus@gmail.com>
2025-09-11 10:23:25 +0900

    вызов is_user_authorized теперь асинхронный (await);
    меню строится через компактную функцию get_menu_keyboard();
    
    логирование аккуратнее, добавлено предупреждение при неавторизованном доступе.

--- handlers/user.py
+++ handlers/user.py
@@ -1,4 +1,4 @@
-from aiogram import Router, F
+from aiogram import Router
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram.filters import Command
 from db import is_user_authorized
@@ -6,19 +6,24 @@ from logger import logger, set_chat_id
 
 router = Router()
 
+
 def get_menu_keyboard():
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Обратиться в техническую поддержку"))
-    keyboard.add(KeyboardButton("Позвонить в техподдержку"))
-    return keyboard
+    return ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        keyboard=[
+            [KeyboardButton(text="Обратиться в техническую поддержку")],
+            [KeyboardButton(text="Позвонить в техподдержку")],
+        ],
+    )
+
 
 @router.message(Command("menu"))
 async def cmd_menu(message: Message):
     set_chat_id(message.from_user.id)
-    if not is_user_authorized(message.from_user.id):
+    if not await is_user_authorized(message.from_user.id):
         logger.warning("Unauthorized user attempted to access menu")
         await message.answer("Пожалуйста, авторизуйтесь с помощью /start.")
         return
-    
+
     logger.info("Menu accessed")
     await message.answer("Выберите действие:", reply_markup=get_menu_keyboard())
