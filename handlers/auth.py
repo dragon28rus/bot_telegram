5cd66a8b5acd65ae53116c1f8aedea1840a51854
Бикмурзин Александр <dragon28rus@gmail.com>
2025-09-12 13:40:18 +0900

    провка импортов

--- handlers/auth.py
+++ handlers/auth.py
@@ -1,5 +1,6 @@
 from typing import Optional, Dict, Any
 from aiogram import Router, F
+from aiogram.filters import Command
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
