51ac13cce8b26d68030102f30697cf94b3add825
Бикмурзин Александр <dragon28rus@gmail.com>
2025-09-19 18:05:07 +0900

    изменение клавиатуры

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,4 +1,4 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardBuilder, types
 from aiogram import Router
 from db.users import get_user_by_chat_id
 from config import SUPPORT_CHAT_ID
@@ -12,8 +12,32 @@ async def get_main_menu(chat_id: int) -> ReplyKeyboardMarkup:
     """
     user = await get_user_by_chat_id(chat_id)
 
+    builder = ReplyKeyboardBuilder()
+
     if user and user.get("contract_id"):
         # Авторизованный пользователь
+        builder.row(
+            types.KeyboardButton(text="💰 Узнать баланс"),
+            types.KeyboardButton(text="📊 Текущий тариф")
+        )
+        builder.row(
+            types.KeyboardButton(text="💳 Последние платежи"),
+            types.KeyboardButton(text="💵 Оплатить услуги")
+        )
+        builder.row(
+            types.KeyboardButton(text="📰 Новости"),
+            types.KeyboardButton(text="✉️ Техподдержка")
+        )
+        builder.row(
+            types.KeyboardButton(text="📞 Позвонить в абонентский отдел")
+        )
+        builder.row(
+            types.KeyboardButton(text="📞 Позвонить в техподдержку")
+        )
+        builder.row(
+            types.KeyboardButton(text="🔓 Отвязать договор")
+        )
+        '''
         keyboard = [
             [KeyboardButton(text="💰 Узнать баланс")],
             [KeyboardButton(text="📊 Текущий тариф")],
@@ -25,23 +49,44 @@ async def get_main_menu(chat_id: int) -> ReplyKeyboardMarkup:
             [KeyboardButton(text="📞 Позвонить в техподдержку")],
             [KeyboardButton(text="🔓 Отвязать договор")],
         ]
+        '''
     elif {chat_id} == {SUPPORT_CHAT_ID}:
         # Аккаунт технической поддержки
+        builder.row(
+            types.KeyboardButton(text="✉️ Написать абоненту"),
+            types.KeyboardButton(text="✉️ Масовая рассылка")
+        )
+        '''
         keyboard = [
             [KeyboardButton(text="✉️ Написать абоненту")],
             [KeyboardButton(text="✉️ Масовая рассылка")],
         ]
+        '''
     else:
         # Неавторизованный пользователь
+        builder.row(
+            types.KeyboardButton(text="🔑 Авторизоваться")
+        )
+        builder.row(
+            types.KeyboardButton(text="✉️ Техподдержка")
+        )
+        builder.row(
+            types.KeyboardButton(text="📞 Позвонить в абонентский отдел")
+        )
+        builder.row(
+            types.KeyboardButton(text="📞 Позвонить в техподдержку")
+        )
+        '''
         keyboard = [
             [KeyboardButton(text="🔑 Авторизоваться")],
             [KeyboardButton(text="✉️ Техподдержка")],
             [KeyboardButton(text="📞 Позвонить в абонентский отдел")],
             [KeyboardButton(text="📞 Позвонить в техподдержку")],
         ]
+        '''
 
     return ReplyKeyboardMarkup(
-        keyboard=keyboard,
+        keyboard=builder,
         resize_keyboard=True,
         one_time_keyboard=False
     )
